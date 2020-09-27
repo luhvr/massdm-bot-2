79e2ab5a0b7aa15b12fc746edf37870288945484
terrify <64439264+luhvr@users.noreply.github.com>
2020-09-27 19:45:45 +0200

    Update dmcode.py

--- dmcode.py
+++ dmcode.py
@@ -1,10 +1,30 @@
-@client.command()
+import discord
+import discord.ext import commands
+import discord.ext.commands import Bot
+import asyncio
+
+client.commands.Bot(command_prefix="wtv prefix u want")
+
+client.remove_command("help")
+
+#help command optionable(deletable)
+
+@client.event
+async def on_ready():
+await client.change_presence(activity=discord.Streaming(name="wtv u want", url="enter url"))
+
+
+#Stream cmd was extra yw creds to 3lone
+
+
+@client.command(pass_context=True)
 async def dm(ctx):
     guild = ctx.message.guild
     for member in guild.members:
      await asyncio.sleep(0)
      try:
-       await member.send("put wtv u want")
+       await member.send("wtv u want")
        print("Sent message")
      except:
        pass
+client.run("token")
